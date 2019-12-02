29d3e66e711c88c522090292795b7f03c494e157
Giordon Stark <kratsg@gmail.com>
2019-12-02 10:55:48 -0800

    day 2 part 1 is done

--- 2019/day-02.py
+++ 2019/day-02.py
@@ -45,3 +45,11 @@ assert computer('2,3,0,3,99') == '2,3,0,6,99'
 assert computer('2,4,4,5,99,0') == '2,4,4,5,99,9801'
 assert computer('1,1,1,4,99,5,6,0,99') == '30,1,1,4,2,5,6,0,99'
 assert computer('1,9,10,3,2,3,11,0,99,30,40,50') == '3500,9,10,70,2,3,11,0,99,30,40,50'
+
+program = open('input/day-02.txt').read()
+# To do this, before running the program, replace position 1 with the value 12 and replace position 2 with the value 2.
+temp = program2memory(program)
+temp[1] = 12
+temp[2] = 2
+program = memory2program(temp)
+print(computer(program))
