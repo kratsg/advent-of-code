58f6720319020dd5ee763afac6dea9d832da88df
Giordon Stark <kratsg@gmail.com>
2019-12-02 08:47:22 -0800

    add day 1 part 1

---
+++ 2019/day-01a.py
@@ -0,0 +1,8 @@
+import numpy as np
+module_masses = np.genfromtxt('input/day-01a.txt')
+
+def module_fuel(module_mass):
+  return np.floor_divide(module_mass, 3) - 2
+
+modules_fuel = module_fuel(module_masses)
+print(np.sum(modules_fuel))
