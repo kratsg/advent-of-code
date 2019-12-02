272ae0133152384dd2b518d5843301dc84b9ccba
Giordon Stark <kratsg@gmail.com>
2019-12-02 11:02:54 -0800

    finish up part 2 on day 2

--- 2019/day-02.py
+++ 2019/day-02.py
@@ -1,4 +1,5 @@
 from operator import add, mul
+import sys
 
 OPS = {1: add,
        2: mul}
@@ -46,10 +47,21 @@ assert computer('2,4,4,5,99,0') == '2,4,4,5,99,9801'
 assert computer('1,1,1,4,99,5,6,0,99') == '30,1,1,4,2,5,6,0,99'
 assert computer('1,9,10,3,2,3,11,0,99,30,40,50') == '3500,9,10,70,2,3,11,0,99,30,40,50'
 
+def modify_program(program, noun=12, verb=2):
+  temp = program2memory(program)
+  temp[1] = noun
+  temp[2] = verb
+  return memory2program(temp)
+
+
 program = open('input/day-02.txt').read()
 # To do this, before running the program, replace position 1 with the value 12 and replace position 2 with the value 2.
-temp = program2memory(program)
-temp[1] = 12
-temp[2] = 2
-program = memory2program(temp)
-print(computer(program))
+print(computer(modify_program(program)))
+
+for noun in range(100):
+  for verb in range(100):
+    result = computer(modify_program(program, noun=noun, verb=verb))
+    memory = program2memory(result)
+    if memory[0] == 19690720:
+      print('noun={} verb={}'.format(noun, verb))
+      sys.exit(0)
