c30cf80abf6b5bbbc3a128bbe3d0f52ea7eb0a6b
Giordon Stark <kratsg@gmail.com>
2020-12-01 18:33:29 -0500

    load in day 1

---
+++ 2020/1/code.py
@@ -0,0 +1,14 @@
+# Advent of code Year 2020 Day 1 solution
+# Author = Giordon Stark
+# Date = December 2020
+
+with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
+    input = input_file.read()
+
+
+
+print("Part One : "+ str(None))
+
+
+
+print("Part Two : "+ str(None))
