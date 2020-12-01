331ecedb1b4425628bfe366fd91fec83dbf8d81e
Giordon Stark <kratsg@gmail.com>
2020-12-01 18:50:07 -0500

    finish day 1

--- 2020/1/code.py
+++ 2020/1/code.py
@@ -2,13 +2,22 @@
 # Author = Giordon Stark
 # Date = December 2020
 
+import itertools
+import math
 with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
     input = input_file.read()
+    input = list(map(int, input.split()))
+    doublets = itertools.combinations(input, 2)
+    triplets = itertools.combinations(input, 3)
 
+for doublet in doublets:
+    if sum(doublet) == 2020:
+        break
 
+print(f"Part One : {math.prod(doublet)}")
 
-print("Part One : "+ str(None))
+for triplet in triplets:
+    if sum(triplet) == 2020:
+        break
 
-
-
-print("Part Two : "+ str(None))
+print(f"Part Two : {math.prod(triplet)}")
