ce0a3a02c23ca9a281db2ab2eaa0091dc61469cd
Giordon Stark <kratsg@gmail.com>
2020-12-01 19:24:52 -0500

    add template day python file

---
+++ 2020/template.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    from aocd.models import Puzzle
+
+    test_vals = []
+    assert False
+
+    puz = Puzzle(2020, N)
+
+    puz.answer_a = None
+    print(f'Part 1: {puz.answer_a}')
+
+    puz.answer_b = None
+    print(f'Part 2: {puz.answer_b}')
