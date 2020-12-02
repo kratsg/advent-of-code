4c1b89a7f847d087db829f1999c4d10780750e8d
Giordon Stark <kratsg@gmail.com>
2020-12-01 20:23:30 -0500

    black code

--- 2020/day01.py
+++ 2020/day01.py
@@ -1,24 +1,29 @@
 import itertools
 import math
 
+
 def process_input(data):
     return list(map(int, data.split()))
+
 
 def find_set(items, n=2, total=2020):
     for combination in itertools.combinations(items, n):
         if sum(combination) == total:
             return combination
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     from aocd.models import Puzzle
 
-    test_vals = process_input("""
+    test_vals = process_input(
+        """
 1721
 979
 366
 299
 675
-1456""")
+1456"""
+    )
 
     assert find_set(test_vals, 2) == (1721, 299)
     assert math.prod(find_set(test_vals, 2)) == 514579
@@ -30,7 +35,7 @@ if __name__ == '__main__':
     numbers = list(map(int, puz.input_data.split()))
 
     puz.answer_a = math.prod(find_set(numbers, 2))
-    print(f'Part 1: {puz.answer_a}')
+    print(f"Part 1: {puz.answer_a}")
 
     puz.answer_b = math.prod(find_set(numbers, 3))
-    print(f'Part 2: {puz.answer_b}')
+    print(f"Part 2: {puz.answer_b}")
--- 2020/template.py
+++ 2020/template.py
@@ -1,4 +1,4 @@
-if __name__ == '__main__':
+if __name__ == "__main__":
     from aocd.models import Puzzle
 
     test_vals = []
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     puz = Puzzle(2020, N)
 
     puz.answer_a = None
-    print(f'Part 1: {puz.answer_a}')
+    print(f"Part 1: {puz.answer_a}")
 
     puz.answer_b = None
-    print(f'Part 2: {puz.answer_b}')
+    print(f"Part 2: {puz.answer_b}")
