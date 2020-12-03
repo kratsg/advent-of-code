53bccefc18ab2d4b76101e694184dee7e674be9b
Giordon Stark <kratsg@gmail.com>
2020-12-02 23:37:13 -0500

    update template a little

--- 2020/template.py
+++ 2020/template.py
@@ -6,8 +6,12 @@ if __name__ == "__main__":
 
     puz = Puzzle(2020, N)
 
+    data = process_input(puz.input_data)
+
     puz.answer_a = None
     print(f"Part 1: {puz.answer_a}")
+
+    assert False
 
     puz.answer_b = None
     print(f"Part 2: {puz.answer_b}")
