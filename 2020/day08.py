4ae826a9995224da590bff34a9b43a6a0e506dde
Giordon Stark <kratsg@gmail.com>
2020-12-08 08:23:35 -0500

    clean up code a bit more for day08

--- 2020/day08.py
+++ 2020/day08.py
@@ -3,13 +3,13 @@ def process_input(data):
     return [(x, int(next(it))) for x in it]
 
 
-def computer(instructions):
-    accumulator = 0
-    indices_visited = []
-    pointer = 0
-    while pointer not in indices_visited:
+def computer(instructions, pointers=None, accumulators=None):
+    accumulators = accumulators or [0]
+    pointers = pointers or [0]
+    while True:
+        pointer = pointers[-1]
+        accumulator = accumulators[-1]
         instruction, value = instructions[pointer]
-        indices_visited.append(pointer)
         if instruction == "nop":
             pointer += 1
         elif instruction == "acc":
@@ -18,10 +18,34 @@ def computer(instructions):
         elif instruction == "jmp":
             pointer += value
 
-        if pointer == len(instructions):
-            return (True, accumulator, [])
+        if pointer in pointers:
+            return (False, pointers, accumulators)
 
-    return (False, accumulator, indices_visited)
+        pointers.append(pointer)
+        accumulators.append(accumulator)
+
+        if pointer == len(instructions):
+            return (True, pointers, accumulators)
+
+    return (False, pointers, accumulators)
+
+
+def fix_bug(instructions, pointers, accumulators):
+    for i in range(1, len(pointers)):
+        index = pointers[-i]
+        command, value = instructions[index]
+        if command not in ["nop", "jmp"]:
+            continue
+
+        new_instructions = [*instructions]
+        new_instructions[index] = ("nop" if command == "jmp" else "nop", value)
+        success, new_pointers, new_accumulators = computer(
+            new_instructions, pointers=pointers[:-i], accumulators=accumulators[:-i]
+        )
+        if success:
+            return (success, new_pointers, new_accumulators)
+
+    return (False, pointers, accumulators)
 
 
 if __name__ == "__main__":
@@ -41,25 +65,26 @@ acc +6"""
     assert len(test_vals) == 9
     assert len(test_vals[0]) == 2
 
-    assert computer(test_vals)[1] == 5
+    test_success, test_pointers, test_accumulators = computer(test_vals)
+    assert test_accumulators[-1] == 5
 
     puz = Puzzle(2020, 8)
 
     data = process_input(puz.input_data)
 
-    _, accumulator, indices_visited = computer(data)
+    _, pointers, accumulators = computer(data)
 
-    puz.answer_a = accumulator
+    puz.answer_a = accumulators[-1]
     print(f"Part 1: {puz.answer_a}")
 
-    for index in indices_visited[::-1]:
-        instructions = [*data]
-        command, value = instructions[index]
-        if command in ["nop", "jmp"]:
-            instructions[index] = ("nop" if command == "jmp" else "nop", value)
-        success, accumulator_part2, _ = computer(instructions)
-        if success:
-            break
+    test_success, test_new_pointers, test_new_accumulators = fix_bug(
+        test_vals, test_pointers, test_accumulators
+    )
+    assert test_success
+    assert test_new_accumulators[-1] == 8
 
-    puz.answer_b = accumulator_part2
+    success, new_pointers, new_accumulators = fix_bug(data, pointers, accumulators)
+    assert success
+
+    puz.answer_b = new_accumulators[-1]
     print(f"Part 2: {puz.answer_b}")
