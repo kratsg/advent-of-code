f6b1c021b31fe5de5f9e815206bdf01c75d441ce
Giordon Stark <kratsg@gmail.com>
2021-12-01 09:33:39 -0800

    refactor and clean

--- 2021/day01.py
+++ 2021/day01.py
@@ -1,20 +1,20 @@
 def process_input(data):
     return list(map(int, data.split()))
 
-def moving_window(measurements):
-    measurements = (i for i in measurements)
-    base = next(measurements)
-    try:
-        while True:
-            differential = next(measurements) - base
-            yield 1 if differential > 0 else 0
-            base += differential
-    except StopIteration:
-        pass
+def window(sequence, size=2):
+    it = iter(sequence)
+    result = [next(it) for _ in range(size)]
+    if len(result) == size:
+        yield result
+    for element in it:
+        result = result[1:] + [element]
+        yield result
 
-def window_sum(measurements):
-    for i, base in enumerate(measurements[:-2]):
-        yield sum(measurements[i:i+3])
+def part1(sequence):
+    return sum(map(lambda x: x[-1] > x[0], window(sequence, size=2)))
+
+def part2(sequence):
+    return sum(map(lambda x: x[-1] > x[0], window(sequence, size=6)))
 
 if __name__ == "__main__":
     from aocd.models import Puzzle
@@ -32,17 +32,18 @@ if __name__ == "__main__":
 263"""
     )
 
-    assert sum(moving_window(test_vals)) == 7
+
+    assert part1(test_vals) == 7
 
     puz = Puzzle(2021, 1)
 
     numbers = list(map(int, puz.input_data.split()))
 
-    puz.answer_a = sum(moving_window(numbers))
+    puz.answer_a = part1(numbers)
     print(f"Part 1: {puz.answer_a}")
 
 
-    assert sum(moving_window(window_sum(test_vals))) == 5
+    assert part2(test_vals) == 5
 
-    puz.answer_b = sum(moving_window(window_sum(numbers)))
+    puz.answer_b = part2(numbers)
     print(f"Part 2: {puz.answer_b}")
